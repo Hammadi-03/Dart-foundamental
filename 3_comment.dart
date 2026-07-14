0962cbcbd119c92ad2cbef15c1c2360ecc9b7315
Hammadi-01 <abdulazizhammadi809@gmail.com>
2026-07-14 07:15:47 +0300

    innit

---
+++ 3_comment.dart
@@ -0,0 +1,10 @@
+const num pi = 3.14;
+
+void main(list<String> args) {
+  var radius = 10; //bisa dibuah (mutable) karna ada var
+print('Luaas linkaran dengan radius $radius adalah ${luasLingkaran(radius)}');
+
+final firstNmae = 'Ali'; 
+final lastName = 'Fauzi'; 
+lastName = 'Fauzi'; //tidak bisa diubah (immutable) karna ada final
+}
