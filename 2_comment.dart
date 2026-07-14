be6c2e3d8b635b8d2b1df56c8be164f8b654671b
Hammadi-01 <abdulazizhammadi809@gmail.com>
2026-07-14 06:34:41 +0300

    first commit

---
+++ 2_comment.dart
@@ -0,0 +1,17 @@
+// single Comment
+/// multi line comment
+
+/**Multiline comment 
+ usually used for explianing subeuah functions */
+   
+
+   
+/// [void] 
+
+void main(list<String> args) {
+// hallo ali!
+  print('Hello, Ali!');
+
+  print('object');
+}
+
