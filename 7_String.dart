a88d0182d7db1eb10bd04be3e5121b8d11c4a135
Hammadi-01 <abdulazizhammadi809@gmail.com>
2026-07-15 06:12:09 +0300

    innit

--- 7_String.dart
+++ 7_String.dart
@@ -1,14 +1,13 @@
 void main(List<String> args) {
-String singleQoute = 'Ini adalah single qoute';
-String doubleQoute = "Ini adalah single qoute";
-print(singleQoute);
-print(doubleQoute);
-print("Hello Apa Kabar Ucup?", "Baik, alhamdulillah"');
-print("Hello Apa Kabar Ucup?", "Baik, alhamdulillah"");
-var name = 'Ucup';
-print('Hello my name is $name dengan penghasilan \$2000/month bekerja di a\'inul yakin');
-// dinamakan escape string
-// unicode
-print('\u{1f600}');
-print('\u[1f671}');
-}#1-18 void main(List<String> args)
+  String singleQoute = 'Ini adalah single qoute';
+  String doubleQoute = "Ini adalah double qoute";
+  print(singleQoute);
+  print(doubleQoute);
+  print('Hello Apa Kabar Ucup? Baik, alhamdulillah');
+  var name = 'Khalid';
+  print('Hello my name is $name dengan penghasilan \$700000/month bekerja di a\'IBM');
+  // dinamakan escape string
+  // unicode
+  print('\u{1f600}');
+  print('\u{1f671}');
+}
