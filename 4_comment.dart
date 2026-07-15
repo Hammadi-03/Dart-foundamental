322219f1d56680334e421bbbe2ff6c1e98b93e84
Hammadi-01 <abdulazizhammadi809@gmail.com>
2026-07-15 06:10:44 +0300

    innit

---
+++ 4_comment.dart
@@ -0,0 +1,26 @@
+/**
+
+*Tipe Data adalah sebuah konsep yang digunakan untuk mengklasifikasi
+*jenis data yang dapat disimpan dan diolah dalam sebuah program
+*String = menampung data dalam bentuk 'text'
+*int = menampung data dalam bentuk bilangan bulat
+*double = menampung data bilangan bulat dan bilangan desimal
+*float = menampung data bilangan desimal
+*bool = menampung data dalam bentuk true atau false
+* List = [1,2,3] //daftar nialai
+*Map = {'key':'value'} //key value pair
+*dynamic = menampung data dalam tipe apapun.
+
+
+*/
+var greeting = 'Hello Khlaed'; // String
+var age = 29; // int
+
+void main(List<String> args) {
+  print(greeting);
+  print('I'a \);
+}
+
+
+
+
---
+++ node.dart
@@ -0,0 +1,24 @@
+/**
+controld melakukan blok pada kata yang sama
+
+shift + alt bawah melakukan copy satu baris yang di blok atau satu line
+shift end melakukan blok ke paling kanan dari line saat ini
+shift + home melakukan blok ke paling kiri dari line saat ini
+shift + pgDn melakukan blok ke paling bawah dari line saat ini
+shift pgup melakukan blok ke paling atas dari line saat ini
+ctrl + B to close the sidebar
+ctrl + shift + B to open the sidebar
+alt + arah atas/bawah untuk pindah ke line atas atau bawah
+
+// cd
+// perintah untuk change directory (pindah dari directory)
+//
+// digunakan untuk menentukan folder sekarang
+///
+// digunakan untuk pemisah folder saat ini
+// jadi ketika rtid@DESKTOP-GUCUORN MINGW64 -/Documents/clone
+// dan melakukan cd ../../ maka kita balik lagi ke root
+/**
+perintah
+perintah
+perintah ucup perintah ucup
